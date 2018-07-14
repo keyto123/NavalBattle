828e2a6ab1f15f232eeb6dbd8c48deb339463513
keyto123 <luksfreestyle_m_e_@hotmail.com>
2018-07-14 09:42:00 -0300

    Added the new boat

--- src/game/boats/BoatStorage.java
+++ src/game/boats/BoatStorage.java
@@ -5,7 +5,7 @@ public final class BoatStorage {
 	public BoatStorage() {}
 
 	private BoatType boats[] = new BoatType[] {
-			new BoatType("boat1", 2), new BoatType("boat2", 3),
+			new BoatType("boat1", 2), new BoatType("boat2", 3), new BoatType("boat3", 4),
 	};
 	
 	public BoatType getBoatType(int index) {
